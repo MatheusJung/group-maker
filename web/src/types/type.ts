89b3f0f0e21ea8d9be9c1d8aa437373e9d14fbd3
Matheus Jung <matheusjmg94@gmail.com>
2026-07-13 22:48:27 -0300

    feature(web): Added types grupo, membro, membro do grubo e usuario

---
+++ web/src/types/type.ts
@@ -0,0 +1,33 @@
+export interface GrupoInterface {
+  id: number;
+  nome: string;
+  descricao?: string;
+  maxParticipantes?: number;
+  parentId: number | null;
+}
+
+export interface ParticipanteInterface {
+  id: number;
+  nome: string;
+  foto?: string;
+}
+
+export interface MembroInterface {
+  participanteId: number;
+  grupoId: number;
+  isAdmin: boolean;
+}
+
+export interface UsuarioInterface {
+  id: number;
+  nomeUsuario: string;
+  senha: string;
+}
+
+export type ModalType =
+  | "participante"
+  | "grupo"
+  | "subgrupo"
+  | "usuario"
+  | "login"
+  | null;
